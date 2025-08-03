1ce349478c3c45b1ac000cdee3b577cf6a4ae9ba
Erasmo Navarro <erasmo_navarro.21@hotmail.com>
2025-08-02 23:15:46 -0500

    Se mejoró el formulario para los diferentes tipos de datos.

--- resources/js/pages/tareas/Create.vue
+++ resources/js/pages/tareas/Create.vue
@@ -4,7 +4,8 @@ import Input from '@/components/ui/input/Input.vue';
 import Label from '@/components/ui/label/Label.vue';
 import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
-import { Head } from '@inertiajs/vue3';
+import { Head, useForm } from '@inertiajs/vue3';
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue, } from '@/components/ui/select'
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -12,6 +13,14 @@ const breadcrumbs: BreadcrumbItem[] = [
         href: '/tareas/create',
     },
 ];
+
+const form = useForm({
+    titulo: '',
+    descripcion: '',
+    fecha: '',
+    estado: '',
+    usuario: ''
+})
 </script>
 
 <template>
@@ -23,23 +32,41 @@ const breadcrumbs: BreadcrumbItem[] = [
             <form class="w-8/12 space-y-4">
                 <div class="space-y-2">
                     <Label for="Título de la tarea">Título</Label>
-                    <Input type="text" placeholder="Título"/>
+                    <Input v-model="form.titulo" type="text" placeholder="Título de la tarea"/>
+                    <div class="text-sm text-red-600" v-if="form.errors.titulo">({ form.errors.titulo })</div>
                 </div>
                 <div class="space-y-2">
                     <Label for="Descripción de la tarea">Descripción</Label>
-                    <Input type="text" placeholder="Descripción"/>
+                    <Input type="text" placeholder="Descripción de la tarea"/>
                 </div>
                 <div class="space-y-2">
                     <Label for="Fecha límite de la tarea">Fecha límite</Label>
-                    <Input type="text" placeholder="Fecha límite"/>
+                    <input type="date" id="fecha" name="fecha-limite" value="00-00-0000" min="2025-01-01" max="2030-12-31"/>
                 </div>
                 <div class="space-y-2">
                     <Label for="Estado de la tarea">Estado</Label>
-                    <Input type="text" placeholder="Estado"/>
+                    <Select>
+                        <SelectTrigger>
+                            <SelectValue placeholder="Selecciona el estado de la tarea" />
+                            </SelectTrigger>
+                            <SelectContent>
+                            <SelectGroup>
+                                <SelectItem value="pendiente">
+                                Pendiente
+                                </SelectItem>
+                                <SelectItem value="enprogreso">
+                                En progreso
+                                </SelectItem>
+                                <SelectItem value="completada">
+                                Completada
+                                </SelectItem>
+                            </SelectGroup>
+                            </SelectContent>
+                    </Select>
                 </div>
                 <div class="space-y-2">
                     <Label for="Usuario asignado de la tarea">Usuario</Label>
-                    <!--<Input type="text" placeholder="Estado"/>-->
+                    <Input type="text" placeholder="Usuario asignado de la tarea"/>
                 </div>
             </form>
         </div>
